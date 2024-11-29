28b5f3f0eaa9f5796d84a15399ff58227aa33e59
harvspex <harvspex@protonmail.com>
2024-11-29 16:15:03 +1100

    compares files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.lexicon import Lexicon
+from tests.test_output import compare
 from timeit import timeit
 
 # TODO: Put in /src ?
@@ -10,13 +11,17 @@ def time_build_lexicon(
     lexicon: Lexicon,
     input_filename: str,
     output_filename: str='out.txt',
-    N_REPEATS: int=1
+    N_REPEATS: int=1,
+    compare_files: bool=False
 ):
     print("\nRunning...")
     total_execution_time = timeit(lambda: lexicon.build_lexicon(input_filename, output_filename), globals=globals(), number=N_REPEATS)
     average_time = total_execution_time / N_REPEATS
     print("COMPLETE.")
     print(f'\nAverage execution time: {average_time:.2f}s across {N_REPEATS} runs')
+
+    if compare_files:
+        compare(input_filename, output_filename)
 
 def main():
     input_filename: str = 'in.txt'
