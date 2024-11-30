a0180b3fa5a3ab3f1bc9b4784063535589bee357
harvspex <harvspex@protonmail.com>
2024-11-30 23:29:54 +1100

    test cases

---
+++ tests/test_neighbour_utils.py
@@ -0,0 +1,21 @@
+from utils.neighbours_utils import *
+from src.word import Word
+
+def test_word_is_neighbours():
+    get_word_list = lambda word_list: [Word(word) for word in word_list.split()]
+
+    test_word = Word('cat')
+    neighbour_words = 'bat cut cab'
+    non_neighbour_words = 'cup but bag dog'
+
+    for other_word in get_word_list(neighbour_words):
+        assert word_is_neighbours(test_word, other_word)
+
+    for other_word in get_word_list(non_neighbour_words):
+        assert not word_is_neighbours(test_word, other_word)
+
+def main():
+    test_word_is_neighbours()
+
+if __name__ == '__main__':
+    main()
