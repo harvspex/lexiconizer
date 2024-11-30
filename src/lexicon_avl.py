f7444d1853359dc03455c0acfeb7295486819307
harvspex <harvspex@protonmail.com>
2024-11-30 14:03:28 +1100

    created

---
+++ src/lexicon_avl.py
@@ -0,0 +1,1 @@
+# TODO: Should be a subclass of Lexicon, using a word_tree
---
+++ src/lexicon_dict.py
@@ -0,0 +1,2 @@
+# TODO: Should be a subclass of lexicon, using a dict.
+# This is to compare the speed of the avl version against a builtin version.
