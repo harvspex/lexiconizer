0cefb5c3b35709e8558b30b0d59d206cc6b1ac2c
harvspex <harvspex@protonmail.com>
2024-12-01 13:46:35 +1100

    created

---
+++ src/func_class.py
@@ -0,0 +1,7 @@
+# TODO: Docstring
+class FuncClass:
+    def __init__(self, name, description, *args, **kwargs):
+        self.name = name
+        self.description = description
+        self.args = args
+        self.kwargs = kwargs
