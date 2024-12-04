2160829fe2935deb5c0aef2cf87a71bd1b146695
harvspex <harvspex@protonmail.com>
2024-12-05 10:04:38 +1100

    modified

--- lexiconizer/__main__.py
+++ lexiconizer/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from lexiconizer.lexiconizer import main
+from lexiconizer.cli.cli import main
 
 if __name__ == '__main__':
     main()
--- lexiconizer/lexiconizer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import lexiconizer.cli.cli as cli
-
-def main():
-    cli.main()
-
-if __name__ == '__main__':
-    main()
