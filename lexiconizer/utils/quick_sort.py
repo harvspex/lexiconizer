1281f1c1ce1aa093f277863f45cdc80fe9fffbdc
harvspex <harvspex@protonmail.com>
2024-12-04 12:26:40 +1100

    added type hints

--- lexiconizer/utils/quick_sort.py
+++ lexiconizer/utils/quick_sort.py
@@ -1,19 +1,20 @@
-def quick_sort(lexicon):
-    def swap(lst, idx_a, idx_b):
+def quick_sort(word_list: list[str]):
+
+    def swap(lst: list, idx_a: int, idx_b: int):
         lst[idx_a], lst[idx_b] = lst[idx_b], lst[idx_a]
-      
-    def quick_sort_inner(lexicon, left=None, right=None):
+
+    def quick_sort_inner(word_list: list[str], left: int=None, right: int=None):
         if left is None and right is None:
             left = 0
-            right = len(lexicon) - 1
+            right = len(word_list) - 1
         if left < right:
-            pivot = partition(lexicon, left, right)
-            quick_sort_inner(lexicon, left, pivot-1)
-            quick_sort_inner(lexicon, pivot+1, right)
+            pivot: int = partition(word_list, left, right)
+            quick_sort_inner(word_list, left, pivot-1)
+            quick_sort_inner(word_list, pivot+1, right)
 
-    def partition(sub_list, left, right):
-        mid = (left + right) // 2
-        pivot = sub_list[mid]
+    def partition(sub_list: list[str], left: int, right: int) -> int:
+        mid: int = (left + right) // 2
+        pivot: int = sub_list[mid]
         swap(sub_list, mid, right)
 
         while left < right:
@@ -29,4 +30,4 @@ def quick_sort(lexicon):
                 left += 1
         return left
 
-    quick_sort_inner(lexicon)
+    quick_sort_inner(word_list)
